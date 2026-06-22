6444782af4022735f0948ee1b6b898177c474e01
xav13 <contact@xavierarnal.com>
2026-06-22 17:05:29 +0200

    Update prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -10,7 +10,6 @@ export default defineConfig({
   schema: 'prisma/schema.prisma',
   datasource: {
     url: process.env.DATABASE_URL!,
-    directUrl: process.env.DIRECT_URL,
   },
   experimental: {
     externalTables: true,
